87f43d1b5c56ee5bda27754c4871bc7873285991
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-11-08 01:38:40 +0000

    feat: adding translation for countdown

--- src/components/countdowns/CountdownEvent.svelte
+++ src/components/countdowns/CountdownEvent.svelte
@@ -49,6 +49,11 @@
   let now = dayjs().valueOf();
   let end = dayjs(endTime).valueOf();
 
+  export let dayVal = "hari";
+  export let hourVal = "jam";
+  export let minuteVal = "menit";
+  export let secondVal = "detik";
+
   onMount(() => {
     timer = setInterval(() => {
       now = dayjs().valueOf();
@@ -77,20 +82,20 @@
 <div class="text-6xl text-center flex w-full items-center justify-center">
   <div class="w-24 mx-1 p-2 bg-white text-[#fbbc05] rounded-lg">
     <div class="font-mono leading-none" x-text="days">{d}</div>
-    <div class="font-mono uppercase text-sm leading-none">Hari</div>
+    <div class="font-mono uppercase text-sm leading-none">{dayVal}</div>
   </div>
   <div class="w-24 mx-1 p-2 bg-white text-[#fbbc05] rounded-lg">
     <div class="font-mono leading-none" x-text="hours">{h}</div>
-    <div class="font-mono uppercase text-sm leading-none">Jam</div>
+    <div class="font-mono uppercase text-sm leading-none">{hourVal}</div>
   </div>
   <div class="w-24 mx-1 p-2 bg-white text-[#fbbc05] rounded-lg">
     <div class="font-mono leading-none" x-text="minutes">{m}</div>
-    <div class="font-mono uppercase text-sm leading-none">Menit</div>
+    <div class="font-mono uppercase text-sm leading-none">{minuteVal}</div>
   </div>
   <div class="text-2xl mx-1 font-extralight text-slate-500">dan</div>
   <div class="w-24 mx-1 p-2 bg-white text-[#fbbc05] rounded-lg">
     <div class="font-mono leading-none" x-text="seconds">{s}</div>
-    <div class="font-mono uppercase text-sm leading-none">Detik</div>
+    <div class="font-mono uppercase text-sm leading-none">{secondVal}</div>
   </div>
 </div>
 
