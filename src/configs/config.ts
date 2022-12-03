797ea5a8a22b0b2b168eb87c5a1af65a13775766
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-12-03 05:12:15 +0000

    feat: change end time to realtime

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,2 +1,2 @@
 export const eventTimeStart: string = "2022-11-08T00:00:00.000-07:00";
-export const eventTimeEnd: string = "2022-12-04T00:00:00.000+07:00";
+export const eventTimeEnd: string = "2022-12-05T00:00:00.000+07:00";
